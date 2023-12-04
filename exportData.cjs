2b832f8c3b0fdb125f6a4f9328d687716e8506a2
Ruben Ellefsen <ellefsen88@gmail.com>
2023-12-04 16:15:35 +0100

    add info132h21 exam

--- exportData.cjs
+++ exportData.cjs
@@ -1,6 +1,6 @@
 const admin = require('firebase-admin');
 const serviceAccount = require('../../../../ruben/Development/Examify/ExamifyFirebaseAdmin.json');
-const data = require('./tester.json');
+const data = require('./info132h21.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
@@ -9,7 +9,7 @@ admin.initializeApp({
 const db = admin.firestore();
 
 data.forEach((item, index) => {
-  db.collection('tester').doc(`question${index}`).set(item) // Set collection name here, and what to call each entry
+  db.collection('info132h21').doc(`question${index}`).set(item) // Set collection name here, and what to call each entry
     .then(() => console.log(`Document question${index} successfully written!`))
     .catch((error) => console.error("Error writing document: ", error));
 });
